1ef309215523aadbdab0e6a7cb47dc3297445f30
Eric <eric.wolter@gmx.de>
2018-11-04 21:34:55 +0100

    updated test stuff

--- test_mobilenet.py
+++ test_mobilenet.py
@@ -8,25 +8,32 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 
-img_path = 'data/elephant.jpg'
+img_path = 'data/eval1A.jpg'
 img = keras.preprocessing.image.load_img(img_path, target_size=(224, 224))
 x = keras.preprocessing.image.img_to_array(img)
 x = keras.applications.mobilenet_v2.preprocess_input(x)
-print(x)
+# print(x)
+
+# model = keras.applications.MobileNetV2(weights='imagenet')
+
+# preds = model.predict(x)
+# print('Predicted:', keras.applications.mobilenet_v2.decode_predictions(preds, top=3)[0])
 
 tf.enable_eager_execution()
 
-with open('data/elephant.jpg', mode='rb') as file:
+with open('data/eval1A.jpg', mode='rb') as file:
     value = file.read()
 
 t = tf.image.decode_jpeg(value)
+print('DECODE')
+tf.Print(t, [t], summarize=224*224*3)
+t = tf.divide(t, 128.)
+print('DIVIDE')
+tf.Print(t, [t], summarize=224*224*3)
+t = tf.subtract(t, 1.)
+print('SUBTRACT')
+tf.Print(t, [t], summarize=224*224*3)
+print('FLOAT')
 t = tf.image.convert_image_dtype(t, dtype=tf.float32)
-t = (t - 0.5) * 2
-print(t)
-
-model = keras.applications.MobileNetV2(weights='imagenet')
-
-preds = model.predict(t)
-print('Predicted:', keras.applications.mobilenet_v2.decode_predictions(preds, top=3)[0])
-
+tf.Print(t, [t], summarize=224*224*3)
 
---
+++ test_reader.py
@@ -0,0 +1,56 @@
+# pylint: disable=line-too-long
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+from tensorflow import keras
+from PIL import Image
+
+tf.enable_eager_execution()
+
+dataset = tf.data.TFRecordDataset(['data/eval.tfrecords'])
+def _parse_function(example):
+    features = {'img1_raw': tf.FixedLenFeature((), tf.string, default_value=""),
+                'img2_raw': tf.FixedLenFeature((), tf.string, default_value=""),
+                'ratio_1>2': tf.FixedLenFeature((), tf.float32, default_value=0)}
+    parsed_features = tf.parse_single_example(example, features)
+
+    img1 = tf.image.decode_jpeg(parsed_features['img1_raw'], channels=3)
+    img1 = tf.image.convert_image_dtype(img1, dtype=tf.float32)
+    img1 = (img1 - 0.5) * 2
+
+    img2 = tf.image.decode_jpeg(parsed_features['img2_raw'], channels=3)
+    img2 = tf.image.convert_image_dtype(img2, dtype=tf.float32)
+    img2 = (img2 - 0.5) * 2
+
+    ratio = parsed_features['ratio_1>2']
+
+    features = {'input1': img1,
+                'input2': img2}
+    labels = [ratio, 1 - ratio]
+
+    return features, labels
+
+dataset = dataset.map(_parse_function)
+iterator = dataset.make_one_shot_iterator() 
+
+record1 = iterator.next()
+print(record1)
+# A1=record1[0]['input1'].numpy()
+# B1=record1[0]['input2'].numpy()
+
+# im = Image.fromarray(A1)
+# im.save("data/eval1A.jpg")
+# im = Image.fromarray(B1)
+# im.save("data/eval1B.jpg")
+
+# record2 = iterator.next()
+# A2=record2[0]['input1'].numpy()
+# B2=record2[0]['input2'].numpy()
+
+# im = Image.fromarray(A2)
+# im.save("data/eval2A.jpg")
+# im = Image.fromarray(B2)
+# im.save("data/eval2B.jpg")
