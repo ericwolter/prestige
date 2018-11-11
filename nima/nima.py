25a5b092d2e4f58849f296814a810df00f36dee9
Eric <eric.wolter@gmx.de>
2018-11-11 18:42:48 +0100

    Adds NIMA baseline implementation

---
+++ nima/nima.py
@@ -0,0 +1,19 @@
+# pylint: disable=line-too-long
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+from tensorflow import keras
+
+m = keras.applications.MobileNetV2(input_shape=(224,224,3), include_top=False, weights='imagenet', pooling='avg')
+
+# add dropout and dense layer
+x = Dropout(0.75)(m.output)
+x = Dense(units=10, activation='softmax')(x)
+
+nima = keras.Model(m.inputs, x)
+nima.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='mean_squared_error',
+              metrics=['accuracy'])
