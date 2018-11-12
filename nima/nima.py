74d24ca64101ac4edb6042b041304640c337aaad
Eric <eric.wolter@gmx.de>
2018-11-12 23:42:11 +0100

    Updates nima model to compile

--- nima/nima.py
+++ nima/nima.py
@@ -10,10 +10,12 @@ from tensorflow import keras
 m = keras.applications.MobileNetV2(input_shape=(224,224,3), include_top=False, weights='imagenet', pooling='avg')
 
 # add dropout and dense layer
-x = Dropout(0.75)(m.output)
-x = Dense(units=10, activation='softmax')(x)
+x = keras.layers.Dropout(0.75)(m.output)
+x = keras.layers.Dense(units=10, activation='softmax')(x)
 
 nima = keras.Model(m.inputs, x)
 nima.compile(optimizer=tf.train.AdamOptimizer(),
               loss='mean_squared_error',
               metrics=['accuracy'])
+
+nima.summary()
